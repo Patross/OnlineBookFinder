5f6699eb41f11587fb2c778445d6df8ca38e5f9c
PatrossDev <contactpatross@gmail.com>
2018-02-07 09:06:33 +0000

    f**k the footer

--- footer.php
+++ footer.php
@@ -3,41 +3,5 @@
 		<h1>&copy; Copyright 2018</h1>
 		<h1>Disclaimer: this website is not real, it's only a college project. Any data stored on the server is not used commercially in any sort of way.</h1>
 		</div>
-
-
-		<div id="container">
-
-
-			<div id="column1">
-				<div class="left">
-					<form action="">
-						<input type="submit" value="Subscribe to receive ocassional emails" name="submit">
-					</form>
-				</div>
-
-				<div class="right">
-					<h1>Made with love by:</h1>
-					<ul>
-						<li>Patryk</li>
-						<li>Tamas</li>
-						<li>John</li>
-					</ul>
-				</div>
-			</div>
-
-			<div id="column2">
-				<div class="left">
-					<form action="">
-						<textarea name="feedback" id="" cols="30" rows="10"></textarea>
-						<input type="submit" value="Send Feedback" name="submit">
-					</form>
-				</div>
-
-				<div class="right">
-
-			</div>
-		
-		</div>
-	</div>
     </body>
 </html>
